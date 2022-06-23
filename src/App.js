088d184e99e8b47367b131010e0a0e8b4ef6b05a
unknown <flucasjs@gmail.com>
2022-06-23 07:53:51 -0700

    Add countdown timer on button click

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import './App.css';
 
 function App() {
   const [text, updateText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isTimeRunning && timeRemaining > 0) {
+        setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 1);
+      } else {
+        setIsTimeRunning(false);
+      }
+    }, 1000);
+  }, [isTimeRunning, timeRemaining])
 
   function handleChange(e) {
     const { value } = e.target;
-
-    updateText(value)
+    updateText(value);
   }
 
   return (
     <div>
         <h1>Speed Typing Game</h1>
         <textarea onChange={handleChange} value={text}/>
-        <h4>Time Remaining: </h4>
-        <button></button>
-        <h1>Word Count:</h1>
+        <h4>Time Remaining: {timeRemaining}</h4>
+        <button onClick={() => setIsTimeRunning(true)}></button>
+        <h1>Word Count: "???"</h1>
     </div>
   );
 }
