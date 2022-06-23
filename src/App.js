fa407133cf19e77c4828e0208d843c16be849fbb
unknown <flucasjs@gmail.com>
2022-06-23 08:53:48 -0700

    Add word counter and restart button

--- src/App.js
+++ src/App.js
@@ -2,23 +2,44 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [text, updateText] = useState("");
+  const STARTING_TIME = 5;
+
+  const [text, setText] = useState("");
   const [timeRemaining, setTimeRemaining] = useState(5);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
   useEffect(() => {
     setTimeout(() => {
       if (isTimeRunning && timeRemaining > 0) {
         setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 1);
-      } else {
-        setIsTimeRunning(false);
+      } else if (timeRemaining === 0) {
+        endGame();
       }
     }, 1000);
-  }, [isTimeRunning, timeRemaining])
+  }, [isTimeRunning, timeRemaining]);
 
   function handleChange(e) {
     const { value } = e.target;
-    updateText(value);
+    setText(value);
+  }
+
+  function countWords(text) {
+    const wordsArr = text.trim().split(" ");
+    console.log(wordsArr.filter(v => v !== "").length)
+    return wordsArr.filter(v => v !== "").length;
+  }
+
+  function startGame() {
+    setText("");
+    setWordCount(0);
+    setTimeRemaining(STARTING_TIME);
+    setIsTimeRunning(true);
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    setWordCount(countWords(text));
   }
 
   return (
@@ -26,8 +47,8 @@ function App() {
         <h1>Speed Typing Game</h1>
         <textarea onChange={handleChange} value={text}/>
         <h4>Time Remaining: {timeRemaining}</h4>
-        <button onClick={() => setIsTimeRunning(true)}></button>
-        <h1>Word Count: "???"</h1>
+        <button onClick={startGame}>Start</button>
+        <h1>Word Count: {wordCount}</h1>
     </div>
   );
 }
