d3e016a18b7c6da52522e8fe29cbbf617eec5924
unknown <flucasjs@gmail.com>
2022-06-23 09:04:12 -0700

    Disable UI elements depending on state of game
    Clicking the start button multiple times actived mulitple timers which
    caused the timer to descend to negative values. Disabling the start
    button after the game has started prevents this behavior. Moreover, the
    textarea will be disabled after the game has ended.

--- src/App.js
+++ src/App.js
@@ -45,9 +45,9 @@ function App() {
   return (
     <div>
         <h1>Speed Typing Game</h1>
-        <textarea onChange={handleChange} value={text}/>
+        <textarea onChange={handleChange} value={text} disabled={!isTimeRunning}/>
         <h4>Time Remaining: {timeRemaining}</h4>
-        <button onClick={startGame}>Start</button>
+        <button onClick={startGame} disabled={isTimeRunning}>Start</button>
         <h1>Word Count: {wordCount}</h1>
     </div>
   );
